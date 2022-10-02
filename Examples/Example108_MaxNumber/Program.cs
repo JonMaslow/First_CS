ae6c4c9baa7da3c9869b517313a9284133b949bc
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-02 15:29:14 +0500

    second lesson

---
+++ Examples/Example108_MaxNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12132;
+int b2 = 2311;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 313;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
+
+//int max = a1;
+
+/*if (max < b1 ) max = b1;
+if (max < c1 ) max = c1;
+
+if (max < a2 ) max = a2;
+if (max < b2 ) max = b2;
+if (max < c2 ) max = c2;
+
+if (max < a3 ) max = a3;
+if (max < b3 ) max = b3;
+if (max < c3 ) max = c3;*/
+
+//Console.WriteLine(max);
+
+
