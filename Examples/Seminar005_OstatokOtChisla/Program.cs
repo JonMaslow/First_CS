251e232155557ec8721bd9f36a0ad9bb38874ced
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-04 14:42:12 +0500

    Последнее число из трёхзначного

---
+++ Examples/Seminar005_OstatokOtChisla/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8*/
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = int.Parse(Console.ReadLine());
+
+if (1000 > number && 99 < number)
+    Console.WriteLine(number % 10);
+else
+    Console.WriteLine("Вы ввели не трёхзначное число");
