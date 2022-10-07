3e009c28f63203f2938cfb53583ce28646769131
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-07 21:29:08 +0500

    Удаляем вторую цифру из трехзначного числа

---
+++ Examples/Seminar102/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98*/
+
+void GetNumber()
+{
+    Random rnd = new Random();
+    int number = rnd.Next(100, 1000);
+    // можно записать так int number = (New Random()).Next(100,1000);
+    string numeric = number.ToString();
+    int result = int.Parse(numeric[0].ToString() + numeric[2].ToString());
+Console.WriteLine($"При удалении второй цифры из числа {number} получаем число {result}");
+
+}
+GetNumber();
+
+    
+
