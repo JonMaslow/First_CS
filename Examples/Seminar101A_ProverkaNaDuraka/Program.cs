f5e02a2b1f38eab372fcfb42b61a8317fd1a0aec
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-08 00:12:33 +0500

    Проверка на дурака без использования функции

--- Examples/Seminar101A_ProverkaNaDuraka/Program.cs
+++ Examples/Seminar101A_ProverkaNaDuraka/Program.cs
@@ -1,2 +1,32 @@
 ﻿// Проверка на дурака
 
+int GetNumber(string message)
+{
+int result = 0;
+bool isCorrect = false;
+
+while(!isCorrect)
+{
+Console.WriteLine(message);
+
+isCorrect = int.TryParse(Console.ReadLine(), out result);
+
+if(!isCorrect)
+Console.WriteLine("Ввели не число, введите корректное число");
+
+Console.WriteLine();
+}
+
+return result;
+}
+
+
+/*
+reference (ссылочные) - string, class, list, arrays, dictionary...
+value (значимые) - datetime, int, enum, struct, decimal, long, double, bool...
+
+стек и управляемая куча
+
+стек - значимые значения
+управляемой куче - ссылочные
+*/
