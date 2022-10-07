8b81fb43de86f21cf20955ee7ed505708f8ad497
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-08 00:07:14 +0500

    два числа проверяются на квадраты друг друга, два решения верный коммит

--- Examples/Seminar105/Program.cs
+++ Examples/Seminar105/Program.cs
@@ -20,3 +20,25 @@ void GetResult(int numberA, int numberB)
 }
 Console.WriteLine("Введите два числа: ");
 GetResult(int.Parse(Console.ReadLine()??""), int.Parse(Console.ReadLine()??""));
+
+/*ЛИБО верный вариант от старого програмиста:
+
+void GetResult(int firstNumeric, int secondNumeric){
+
+
+if(firstNumeric * firstNumeric == secondNumeric){
+Console.WriteLine($"Numeric {secondNumeric} = {firstNumeric}^2");
+return;
+}
+
+if(secondNumeric * secondNumeric == firstNumeric){
+Console.WriteLine($"Numeric {firstNumeric} = {secondNumeric}^2");
+return;
+}
+
+Console.WriteLine($"It's so bad result =(");
+
+}
+
+GetResult(16, 4);
+*/
