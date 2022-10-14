cbc367c1785aa40bd6e27c14afa0aeb4616a7980
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-14 20:22:13 +0500

    Seminar 404 Создан случайный массив со значениями от 0 до 1

---
+++ Examples/Seminar404/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]*/
+
+int [] CreateArray(int n)
+{
+    int [] array = new int [n];
+    Random rnd = new Random ();
+    for (int i = 0; i < array.Length; i++)
+    {
+     array[i] = rnd.Next(0,2);   
+    }
+    return array;
+}
+int[] arr = CreateArray (8);
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+}
+Console.Write("[");
+PrintArray(arr);
+Console.Write("]");
