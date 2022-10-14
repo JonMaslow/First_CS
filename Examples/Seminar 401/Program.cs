6ee968d1cefcc6d60dd14888f9938642406e8583
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-14 18:15:21 +0500

    Seminar 402 Определение количества цифр в числе

---
+++ Examples/Seminar 401/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()??"");
+    return result;
+}
+
+int SummNumber(int numberA)
+{
+    int summ = 0;
+    for (int i = 1; i <= numberA; i++)
+    {
+        summ += i;
+    }
+    return summ;
+}
+
+int numA = GetNumber("Введите число А: ");
+int sumDigit = SummNumber(numA);
+Console.WriteLine ($"Сумма чисел от 1 до {numA} равна {sumDigit}");
---
+++ Examples/Seminar402/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()??"");
+    return result;
+}
+int DigitNumber (int digit)
+{
+    int count = 0;
+    while (digit % 10 != 0)
+    {
+        digit = digit / 10;
+        count ++;
+    } 
+    return count;
+}
+int digit = GetNumber("Введите число А: ");
+int count = DigitNumber(digit);
+Console.WriteLine($"В числе {digit} содержиться {count} цифр");
