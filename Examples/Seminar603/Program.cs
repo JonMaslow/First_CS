4464b04a2f9727a7f991168f3c3e65d3f7faa442
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-25 18:23:39 +0500

    Массив чисел Фибонначчи

---
+++ Examples/Seminar603/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 44: 
+Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+
+*/
+int GetNumberFromConsole()
+{
+    Console.WriteLine("Введите число:");
+    int number = int.Parse(Console.ReadLine()??"");
+    return number;
+}
+int number = GetNumberFromConsole();
+int [] GetFibonachiArray(int number)
+{   
+    int[] array = new int [number];
+    
+        array[0] = 0;
+        array[1] = 1;
+     
+    for (int i = 2; i < array.Length; i++)
+    {   
+       
+        array[i] = array[i-1] + array[i-2];
+    }
+    /*int i = 0;
+    int num = 0;
+    while (count <= number)
+    {
+        array[i] = num;
+        i++;
+        num += 1;
+        count++;
+    }*/
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int [] fibonachiArray = GetFibonachiArray(number);
+PrintArray(fibonachiArray);
